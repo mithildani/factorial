371efb54908c06b97abfd39b86082ea78ce2c7e3
Mithil Dani <danimithil@gmail.com>
2017-07-12 18:17:34 +0530

    init

---
+++ num.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main()
+{
+    int n;
+    printf("\t\t Welcome to Factorial Calculator! \n\n\n\n Enter any integer: ");
+    scanf("%i",&n);
+
+    int i;
+    double ans=1;
+    for (i=1;i<=n;i++)
+    {
+        ans=ans*i;
+    }
+
+    if (ans<4294967295)
+    {
+        long finans=ans;
+        printf("\n\n %i! is %lu \n\n",n,finans);
+    }
+    else
+    {
+        printf("\n\n %i! is %e \n\n",n,ans);
+    }
+}
