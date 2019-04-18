ae3c3f71ada6706bd414609fc3e2f4986b74ad6e
students <danimithil@gmail.com>
2019-04-18 17:54:36 +0530

    ab conflict

--- num.c
+++ num.c
@@ -6,7 +6,7 @@ void main()
     int n;
     printf("\t\t Welcome to Factorial Calculator! \n\n\n\n Enter any integer: ");
     scanf("%i",&n);
-    printf("Mithils and Sid'sCommit\n");
+    printf("Mithils and Sid'sCommit for CSI WORKSHOP!!!!!!\n");
 
     int i;
     double ans=1;
